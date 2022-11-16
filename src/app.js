f4ad2a97a4e974378d7b8c2521e13952b240c5e4
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2022-11-16 16:39:19 -0300

    feat: sign-up

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import joi from "joi";
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
+import { validateSignup } from "./schemas.js";
 
 const app = express();
 
@@ -16,7 +17,7 @@ dotenv.config();
 //MongoClient
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 
-//conectar Mongo
+//mongo
 try {
   await mongoClient.connect();
 } catch (error) {
@@ -24,8 +25,32 @@ try {
 }
 
 const db = mongoClient.db("myWallet");
+const usersCollection = db.collection("users");
+const statementsCollection = db.collection("statements");
 
 //code
+app.post("/sign-up", async (req, res) => {
+  const body = req.body;
+
+  const { error } = validateSignup(body);
+  if (error) {
+    const errors = error.details.map((e) => e.message);
+    return res.status(422).send(errors);
+  }
+
+  const encryptedPassword = bcrypt.hashSync(body.password, 10);
+
+  try {
+    await usersCollection.insertOne({
+      ...body,
+      password: encryptedPassword,
+      lastStatus: Date.now(),
+    });
+    res.sendStatus(200);
+  } catch (error) {
+    res.send(500);
+  }
+});
 
 //port
 app.listen(5000, () => console.log("Server running in port 5000"));
---
+++ src/schemas.js
@@ -0,0 +1,11 @@
+import joi from "joi";
+
+export const validateSignup = (obj) => {
+  const signupSchema = joi.object({
+    name: joi.string().alphanum().min(3).required(),
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+  });
+
+  return signupSchema.validate(obj, { abortEarly: false });
+};
