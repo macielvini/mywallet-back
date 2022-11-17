88d5ecef4a0b9b6bc78efe25822dba5e473a9054
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2022-11-17 15:43:00 -0300

    refactor: routes callback are now controllers

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
 import express from "express";
 import cors from "cors";
-import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
-import joi from "joi";
-import bcrypt from "bcrypt";
-import { v4 as uuid } from "uuid";
-import { validateSignin, validateSignup } from "./schemas.js";
+import { MongoClient } from "mongodb";
+import { signIn, signUp } from "./controllers/auth.controllers.js";
 
 const app = express();
 
@@ -25,74 +22,16 @@ try {
 }
 
 const db = mongoClient.db("myWallet");
-const usersCollection = db.collection("users");
-const statementsCollection = db.collection("statements");
-const sessionsCollection = db.collection("sessions");
+export const usersCollection = db.collection("users");
+export const statementsCollection = db.collection("statements");
+export const sessionsCollection = db.collection("sessions");
 
 //code
 
 //POST
-app.post("/sign-up", async (req, res) => {
-  const body = req.body;
+app.post("/sign-up", signUp);
 
-  const { error } = validateSignup(body);
-  if (error) {
-    const errors = error.details.map((e) => e.message);
-    return res.status(422).send(errors);
-  }
-
-  const encryptedPassword = bcrypt.hashSync(body.password, 10);
-
-  try {
-    await usersCollection.insertOne({
-      ...body,
-      password: encryptedPassword,
-      lastStatus: Date.now(),
-    });
-    res.sendStatus(200);
-  } catch (error) {
-    res.send(500);
-  }
-});
-
-app.post("/sign-in", async (req, res) => {
-  const body = req.body;
-
-  const { error } = validateSignin(body);
-  if (error) {
-    const errors = error.details.map((e) => e.message);
-    return res.status(422).send(errors);
-  }
-
-  try {
-    const user = await usersCollection.findOne({ email: body.email });
-    if (!user) {
-      return res.sendStatus(401);
-    }
-
-    if (!bcrypt.compareSync(body.password, user.password)) {
-      return res.sendStatus(401);
-    }
-
-    const session = await sessionsCollection.findOne({
-      userId: ObjectId(user._id),
-    });
-
-    if (!session?.token) {
-      const newToken = uuid();
-      await sessionsCollection.insertOne({
-        userId: user._id,
-        token: newToken,
-      });
-
-      return res.send({ token: newToken });
-    }
-
-    res.send({ token: session.token });
-  } catch (error) {
-    res.sendStatus(500);
-  }
-});
+app.post("/sign-in", signIn);
 
 //port
 app.listen(5000, () => console.log("Server running in port 5000"));
---
+++ src/controllers/auth.controllers.js
@@ -0,0 +1,68 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
+import { sessionsCollection, usersCollection } from "../app.js";
+import { validateSignin, validateSignup } from "../schemas/authSchemas.js";
+import { ObjectId } from "mongodb";
+
+export const signUp = async (req, res) => {
+  const body = req.body;
+
+  const { error } = validateSignup(body);
+  if (error) {
+    const errors = error.details.map((e) => e.message);
+    return res.status(422).send(errors);
+  }
+
+  const encryptedPassword = bcrypt.hashSync(body.password, 10);
+
+  try {
+    await usersCollection.insertOne({
+      ...body,
+      password: encryptedPassword,
+      lastStatus: Date.now(),
+    });
+    res.sendStatus(200);
+  } catch (error) {
+    res.send(500);
+  }
+};
+
+export const signIn = async (req, res) => {
+  const body = req.body;
+
+  const { error } = validateSignin(body);
+  if (error) {
+    const errors = error.details.map((e) => e.message);
+    return res.status(422).send(errors);
+  }
+
+  try {
+    const user = await usersCollection.findOne({ email: body.email });
+    if (!user) {
+      return res.sendStatus(401);
+    }
+
+    if (!bcrypt.compareSync(body.password, user.password)) {
+      return res.sendStatus(401);
+    }
+
+    const session = await sessionsCollection.findOne({
+      userId: ObjectId(user._id),
+    });
+
+    if (!session?.token) {
+      const newToken = uuid();
+      await sessionsCollection.insertOne({
+        userId: user._id,
+        token: newToken,
+      });
+
+      return res.send({ token: newToken });
+    }
+
+    res.send({ token: session.token });
+  } catch (error) {
+    res.sendStatus(500);
+  }
+};
