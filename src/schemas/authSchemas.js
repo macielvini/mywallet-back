dfc3dbfa489321410634a4797a2b6ff1c2206b54
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2022-11-17 23:39:18 -0300

    feat: password confirmation

--- src/schemas/authSchemas.js
+++ src/schemas/authSchemas.js
@@ -5,6 +5,12 @@ export const signupSchema = (obj) => {
     name: joi.string().alphanum().min(3).required(),
     email: joi.string().email().required(),
     password: joi.string().required(),
+    password_confirmation: joi
+      .any()
+      .equal(joi.ref("password"))
+      .required()
+      .label("Confirm password")
+      .options({ messages: { "any.only": "{{#label}} does not match" } }),
   });
 
   return signupSchema.validate(obj, { abortEarly: false });
@@ -13,7 +19,7 @@ export const signupSchema = (obj) => {
 export const signinSchema = (obj) => {
   const signinSchema = joi.object({
     email: joi.string().email().required(),
-    password: joi.string().required(),
+    password: joi.string().required().label("Password"),
   });
 
   return signinSchema.validate(obj, { abortEarly: false });
